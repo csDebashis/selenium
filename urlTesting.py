295f47a03db797e59e689d9a2af515db75da45ad
Debashis <debashis.gcect@gmail.com>
2018-10-31 09:29:04 +0530

    initial commit

---
+++ urlTesting.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options  
+
+import time
+
+
+
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+#chrome_options.binary_location = '/Applications/Google Chrome   Canary.app/Contents/MacOS/Google Chrome Canary'`    
+
+#driver = webdriver.Chrome(executable_path=os.path.abspath(“chromedriver"),   chrome_options=chrome_options)  
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+ 
+#driver = webdriver.Chrome()
+driver.get("https://google.com")
+
+#checking if it is error page or not
+try:
+	element = driver.find_element_by_id("main-frame-error")
+	print("This site can’t be reached")
+except NoSuchElementException:
+	print("Able to open the website")
+except:
+	print("some other exception")	
+
+	
+	
+#filling the form
+try:
+	inputbox = driver.find_element_by_id("lst-ib")
+	result = inputbox.send_keys("some text")
+	btn = driver.find_element_by_name('btnK')
+	btn.click()
+	print("clicked on search button")
+	
+	
+except NoSuchElementException:
+	print ("Unable to find element")
+	
+
+	
